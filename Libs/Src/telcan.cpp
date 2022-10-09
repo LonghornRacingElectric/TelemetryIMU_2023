b9f1f249ce91230faa74963ed995cc88911d8be2
matt-mekha <matthew.mekha@gmail.com>
2022-10-09 18:49:05 -0500

    telcan receive structure

--- Libs/Src/telcan.cpp
+++ Libs/Src/telcan.cpp
@@ -3,38 +3,45 @@
 class TelemetryTx {
 	public:
 		TelemetryTx(uint16_t id);
-		void addByte(uint8_t byte);
-		void addBytes(uint8_t bytes[], uint8_t len);
-		void send();
+		void send(TelemetryTxPacket *packet);
 	private:
 		CAN_TxHeaderTypeDef header;
-		uint8_t data[8];
 };
 
-TelemetryTx::TelemetryTx(uint16_t id) {
+TelemetryTx::TelemetryTX(uint16_t id) {
 	HAL_CAN_Start(&hcan1);
 
 	header.StdId = id;
 	header.ExtId = 0;
 	header.IDE = CAN_ID_STD;
 	header.RTR = CAN_RTR_DATA;
-	header.DLC = 0;
 }
 
-void TelemetryTx::addByte(uint8_t byte) {
-	if(header.DLC == 8) return;
-	data[header.DLC++] = byte;
+void TelemetryTx::send(TelemetryTxPacket *packet) {
+	header.DLC = sizeof(data);
+	HAL_CAN_AddTxMessage(&hcan1, &header, &packet->data, &CAN_TX_MAILBOX0);
 }
 
-void TelemetryTx::addBytes(uint8_t bytes[]) {
+class TelemetryTxPacket {
+	public:
+		void writeByte(uint8_t byte);
+		void writeBytes(uint8_t *bytes[]);
+	private:
+		uint8_t ptr = 0;
+		uint8_t data[8];
+};
+
+
+void TelemetryTxPacket::writeByte(uint8_t byte) {
+	if(ptr == 8) return;
+	data[ptr++] = byte;
+}
+
+void TelemetryTxPacket::writeBytes(uint8_t bytes[]) {
 	uint8_t len = sizeof(bytes);
 	for(uint8_t i = 0; i < len; i++) {
-		this->addByte(bytes[i]);
+		addByte(bytes[i]);
 	}
-}
-
-void TelemetryTx::send() {
-	HAL_CAN_AddTxMessage(&hcan1, &header, &data, &CAN_TX_MAILBOX0);
 }
 
 
@@ -42,5 +49,29 @@ void TelemetryTx::send() {
 
 
 class TelemetryRx {
-
+	public:
+		TelemetryRx(uint16_t ids[]);
+		void receive(TelemetryRxPacket *packet);
 };
+
+void TelemetryRx::receive(TelemetryRxPacket *packet) {
+	// TODO
+}
+
+class TelemetryRxPacket {
+	public:
+		void readByte(uint8_t *byte);
+		void readBytes(uint8_t *bytes[]);
+};
+
+
+void TelemetryTxPacket::readByte(uint8_t *byte) {
+	*byte = 255; // TODO
+}
+
+void TelemetryTxPacket::readBytes(uint8_t *bytes[]) {
+	uint8_t len = sizeof(bytes);
+	for(uint8_t i = 0; i < len; i++) {
+		readByte(bytes + i);
+	}
+}
