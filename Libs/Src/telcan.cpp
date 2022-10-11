427a0b7a120e601ba462e944b6d5cd9a619127c8
matt-mekha <matthew.mekha@gmail.com>
2022-10-11 01:57:40 -0500

    tx packets complete

--- Libs/Src/telcan.cpp
+++ Libs/Src/telcan.cpp
@@ -1,77 +1,143 @@
 #include "telcan.h"
 
-class TelemetryTx {
+
+extern CAN_HandleTypeDef hcan1;
+
+
+class TelemetryCanTxPacket {
 	public:
-		TelemetryTx(uint16_t id);
-		void send(TelemetryTxPacket *packet);
+		void writeByte(uint8_t byte);
+		uint8_t getData();
+		uint8_t data[8];
+	private:
+		uint8_t ptr = 0;
+};
+
+
+void TelemetryCanTxPacket::writeByte(uint8_t byte) {
+	if(ptr == 8) return;
+	data[ptr++] = byte;
+}
+
+class TelemetryCanTxPacket_IMU_Accel : private TelemetryCanTxPacket {
+	public:
+		TelemetryCanTxPacket_IMU_Accel(uint16_t accelX, uint16_t accelY, uint16_t accelZ) {
+			writeByte(TELCAN_PACKET_TYPE_ACCEL << 4);
+			writeByte(accelX >> 8);
+			writeByte(accelX & 0xFF);
+			writeByte(accelY >> 8);
+			writeByte(accelY & 0xFF);
+			writeByte(accelZ >> 8);
+			writeByte(accelZ & 0xFF);
+		}
+};
+
+class TelemetryCanTxPacket_IMU_Gyro : private TelemetryCanTxPacket {
+	public:
+		TelemetryCanTxPacket_IMU_Gyro(uint16_t gyroX, uint16_t gyroY, uint16_t gyroZ) {
+			writeByte(TELCAN_PACKET_TYPE_GYRO << 4);
+			writeByte(gyroX >> 8);
+			writeByte(gyroX & 0xFF);
+			writeByte(gyroY >> 8);
+			writeByte(gyroY & 0xFF);
+			writeByte(gyroZ >> 8);
+			writeByte(gyroZ & 0xFF);
+		}
+};
+
+class TelemetryCanTxPacket_IMU_Temp : private TelemetryCanTxPacket {
+	public:
+		TelemetryCanTxPacket_IMU_Temp(uint8_t temp) {
+			writeByte(TELCAN_PACKET_TYPE_TEMP << 4);
+			writeByte(temp);
+		}
+};
+
+class TelemetryCanTxPacket_IMU_Accel_HighRes : private TelemetryCanTxPacket {
+	public:
+		TelemetryCanTxPacket_IMU_Accel_HighRes(uint32_t accelX, uint32_t accelY, uint32_t accelZ) {
+			writeByte((TELCAN_PACKET_TYPE_ACCEL_HIGHRES << 4) | (accelX >> 16));
+			writeByte((accelX >> 8) & 0xFF);
+			writeByte(accelX & 0xFF);
+			writeByte(accelY >> 12);
+			writeByte((accelY >> 8) & 0xFF);
+			writeByte(((accelY << 4) & 0xF0) | (accelZ >> 16));
+			writeByte((accelZ >> 8) & 0xFF);
+			writeByte(accelZ & 0xFF);
+		}
+};
+
+class TelemetryCanTxPacket_IMU_Gyro_HighRes : private TelemetryCanTxPacket {
+	public:
+		TelemetryCanTxPacket_IMU_Gyro_HighRes(uint32_t gyroX, uint32_t gyroY, uint32_t gyroZ) {
+			writeByte((TELCAN_PACKET_TYPE_GYRO_HIGHRES << 4) | (gyroX >> 16));
+			writeByte((gyroX >> 8) & 0xFF);
+			writeByte(gyroX & 0xFF);
+			writeByte(gyroY >> 12);
+			writeByte((gyroY >> 8) & 0xFF);
+			writeByte(((gyroY << 4) & 0xF0) | (gyroZ >> 16));
+			writeByte((gyroZ >> 8) & 0xFF);
+			writeByte(gyroZ & 0xFF);
+		}
+};
+
+class TelemetryCanTxPacket_IMU_Temp_HighRes : private TelemetryCanTxPacket {
+	public:
+		TelemetryCanTxPacket_IMU_Temp_HighRes(uint16_t temp) {
+			writeByte(TELCAN_PACKET_TYPE_TEMP_HIGHRES << 4);
+			writeByte(temp >> 8);
+			writeByte(temp & 0xFF);
+		}
+};
+
+
+
+class TelemetryCanRxPacket {
+	public:
+		void readByte(uint8_t *byte);
+		CAN_RxHeaderTypeDef header;
+		uint8_t data[8];
+	private:
+		uint8_t ptr = 0;
+};
+
+
+void TelemetryCanRxPacket::readByte(uint8_t *byte) {
+	*byte = data[ptr++];
+}
+
+
+
+
+
+class TelemetryCan {
+	public:
+		TelemetryCan(uint8_t groupId, uint8_t uniqueId);
+		void send(TelemetryCanTxPacket *packet);
+		void receive(TelemetryCanRxPacket *packet);
 	private:
 		CAN_TxHeaderTypeDef header;
 };
 
-TelemetryTx::TelemetryTX(uint16_t id) {
+TelemetryCan::TelemetryCan(uint8_t groupId, uint8_t uniqueId) {
 	HAL_CAN_Start(&hcan1);
 
-	header.StdId = id;
+	header.StdId = (groupId << 4) | uniqueId;
 	header.ExtId = 0;
 	header.IDE = CAN_ID_STD;
 	header.RTR = CAN_RTR_DATA;
 }
 
-void TelemetryTx::send(TelemetryTxPacket *packet) {
-	header.DLC = sizeof(data);
-	HAL_CAN_AddTxMessage(&hcan1, &header, &packet->data, &CAN_TX_MAILBOX0);
+void TelemetryCan::send(TelemetryCanTxPacket *packet) {
+	header.DLC = sizeof(packet->data);
+	HAL_CAN_AddTxMessage(&hcan1, &header, packet->data, (uint32_t*) CAN_TX_MAILBOX0);
 }
 
-class TelemetryTxPacket {
-	public:
-		void writeByte(uint8_t byte);
-		void writeBytes(uint8_t *bytes[]);
-	private:
-		uint8_t ptr = 0;
-		uint8_t data[8];
-};
-
-
-void TelemetryTxPacket::writeByte(uint8_t byte) {
-	if(ptr == 8) return;
-	data[ptr++] = byte;
-}
-
-void TelemetryTxPacket::writeBytes(uint8_t bytes[]) {
-	uint8_t len = sizeof(bytes);
-	for(uint8_t i = 0; i < len; i++) {
-		addByte(bytes[i]);
+void TelemetryCan::receive(TelemetryCanRxPacket *packet) {
+	uint32_t num_messages = 0;
+	while(num_messages == 0) {
+		num_messages = HAL_CAN_GetRxFifoFillLevel(&hcan1, CAN_RX_FIFO0);
 	}
-}
 
-
-
-
-
-class TelemetryRx {
-	public:
-		TelemetryRx(uint16_t ids[]);
-		void receive(TelemetryRxPacket *packet);
-};
-
-void TelemetryRx::receive(TelemetryRxPacket *packet) {
-	// TODO
-}
-
-class TelemetryRxPacket {
-	public:
-		void readByte(uint8_t *byte);
-		void readBytes(uint8_t *bytes[]);
-};
-
-
-void TelemetryTxPacket::readByte(uint8_t *byte) {
-	*byte = 255; // TODO
-}
-
-void TelemetryTxPacket::readBytes(uint8_t *bytes[]) {
-	uint8_t len = sizeof(bytes);
-	for(uint8_t i = 0; i < len; i++) {
-		readByte(bytes + i);
-	}
+	HAL_CAN_GetRxMessage(&hcan1, CAN_RX_FIFO0, &packet->header, packet->data);
 }
